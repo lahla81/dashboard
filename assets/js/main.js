5f9685ffb75fd16ce728a11cf8838a360ba0f5ab
lahla <lahla81@yahoo.com>
2018-12-07 15:19:24 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,114 @@
+$(document).ready(function () {
+
+    // sidebar toggle
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+    (function(){
+    
+        var ctx = document.getElementById("bar-chart").getContext('2d');
+        gradient1 = ctx.createLinearGradient(0, 0, 0, 600);
+
+        gradient1.addColorStop(0, '#BE72FF');
+        gradient1.addColorStop(1, '#7B0ED9');
+
+        gradient2 = ctx.createLinearGradient(0, 0, 0, 600);
+
+        gradient2.addColorStop(0, '#FF95DA');
+        gradient2.addColorStop(1, '#E50497');
+        
+        Chart.defaults.global.defaultFontFamily = " 'JF-Flat-regular', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
+        Chart.defaults.global.defaultFontSize = 20;
+        
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: [ "نسبة الإنجاز الحالية", "نسبة الإنجاز السابقة"],
+                datasets: [{
+                    // label: '# of Votes',
+                    data: [88, 50],
+                    backgroundColor: [ 
+                        gradient2,gradient1
+                    ], 
+                    hoverBackgroundColor: [
+                        gradient2,gradient1
+                    ],
+                    barPercentage: 0.5,
+                    borderWidth: 0,
+
+                    shadowOffsetX: 3,
+                    shadowOffsetY: 3,
+                    shadowBlur: 20,
+                    shadowColor: ['#FF07A9','#850CED']
+                }]
+            },
+            options: {
+                legend: {
+                    display : false
+                },
+                scales: {
+                    pointLabels :{
+                        fontColor: "red",
+                    },
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        },
+                        gridLines: {
+                            color: '#E2E2E2',
+                            zeroLineColor: '#707070',
+                            zeroLineWidth: 1
+                        }
+                    }],
+                    xAxes: [{
+                        ticks: {
+                            fontColor: ['#FF07A9']
+                        },
+                        gridLines: {
+                            display:false,
+                            zeroLineWidth: 4,
+                        },
+                    barPercentage: 1.0,
+                            categoryPercentage: 0.3
+                    }]
+                },
+                tooltips: {
+                enabled: false
+                }          
+            }
+        });
+
+    })();
+
+    $('.checkbox-small').addClass("checked-lines");
+    $('#customCheck-lines:input').click(function(){
+        $('.checkbox-small').addClass("checked-lines");
+        $('.checkbox-small').removeClass("checked-curve");
+        $('.checkbox-small').removeClass("checked-half");
+        $('.checkbox-small').removeClass("checked-circle");
+    });
+
+    $('#customCheck-curve:input').click(function(){
+        $('.checkbox-small').addClass("checked-curve");
+        $('.checkbox-small').removeClass("checked-lines");
+        $('.checkbox-small').removeClass("checked-half");
+        $('.checkbox-small').removeClass("checked-circle");
+    });
+
+    $('#customCheck-half:input').click(function(){
+        $('.checkbox-small').addClass("checked-half");
+        $('.checkbox-small').removeClass("checked-lines");
+        $('.checkbox-small').removeClass("checked-curve");
+        $('.checkbox-small').removeClass("checked-circle");
+    });
+
+    $('#customCheck-circle:input').click(function(){
+        $('.checkbox-small').addClass("checked-circle");
+        $('.checkbox-small').removeClass("checked-half");
+        $('.checkbox-small').removeClass("checked-lines");
+        $('.checkbox-small').removeClass("checked-curve");
+    });
+
+
+});
