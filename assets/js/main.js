43d621edb90e5509daf55ea50a5e7e05732a5685
lahla <lahla81@yahoo.com>
2018-12-07 20:04:28 +0200

    add small line chart

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,8 +34,6 @@ $(document).ready(function () {
                     hoverBackgroundColor: [
                         gradient2,gradient1
                     ],
-                    barPercentage: 0.5,
-                    borderWidth: 0,
 
                     shadowOffsetX: 3,
                     shadowOffsetY: 3,
@@ -115,12 +113,120 @@ $(document).ready(function () {
 
     })();
 
+
+    (function(){
+        var ctx = document.getElementById('curve-chart').getContext('2d');
+        // data = JSON.parse(ctx.parentElement.dataset.values);
+        gradient1 = ctx.createLinearGradient(0, 0, 0, 600);
+
+        gradient1.addColorStop(0, '#BE72FF');
+        gradient1.addColorStop(1, '#7B0ED9');
+
+        gradient2 = ctx.createLinearGradient(0, 0, 0, 600);
+
+        gradient2.addColorStop(0, '#FF95DA');
+        gradient2.addColorStop(1, '#E50497');
+
+        gradient3 = ctx.createLinearGradient(0, 0, 0, 600);
+
+        gradient3.addColorStop(1, 'rgba(59,134,255,0)');
+        gradient3.addColorStop(0, '#3B86FF');
+
+        Chart.defaults.global.defaultFontFamily = " 'JF-Flat-regular', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
+
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+        
+            // The data for our dataset
+            data: {
+                labels: [ "", "نسبة الإنجاز الحالية", "" , "نسبة الإنجاز السابقة",""],
+                datasets: [{
+                    label: "My First dataset",
+                    backgroundColor: gradient3,
+                    borderColor: gradient3,
+                    data: [0 , 88 , 0 , 50 , 0],
+                    lineTension : 0.2
+                }]
+            },
+        
+            // Configuration options go here
+            options: {
+                layout: {
+                    padding: {
+                        left: 10,
+                        right: 10,
+                        top: 30,
+                        bottom: 10
+                    }
+                },
+                legend: {
+                    display : false
+                },
+                plugins: {
+                    
+                },
+                "animation": {
+                    "duration": 1,
+                    "onComplete": function() {
+                      var chartInstance = this.chart,
+                        ctx = chartInstance.ctx;
+              
+                      ctx.font = Chart.helpers.fontString(25, 700, Chart.defaults.global.defaultFontFamily);
+                      ctx.textAlign = 'center';
+                      ctx.textBaseline = 'bottom';
+                      ctx.fillStyle = "white";
+              
+                      this.data.datasets.forEach(function(dataset, i) {
+                        var meta = chartInstance.controller.getDatasetMeta(i);
+                        meta.data.forEach(function(line, index) {
+                          var data = dataset.data[index];
+                          ctx.fillText(data, line._model.x, line._model.y + 35);
+                        });
+                      });
+                    }
+                  },
+                scales: {
+                    pointLabels :{
+                        fontColor: "red",
+                    },
+                    yAxes: [{
+                        ticks: {
+                            display: false,
+                            beginAtZero:true
+                        },
+                        gridLines: {
+                            color: '#E2E2E2',
+                            zeroLineColor: '#707070',
+                            zeroLineWidth: 1
+                        }
+                    }],
+                    xAxes: [{
+                        ticks: {
+                            fontColor: ['#FF07A9']
+                        },
+                        gridLines: {
+                            display:false,
+                            zeroLineWidth: 4,
+                        },
+                
+                    }]
+                },
+                tooltips: {
+                enabled: false
+                }          
+            }
+        });
+    })();
+
     $('.checkbox-small').addClass("checked-lines");
     $('#customCheck-lines:input').click(function(){
         $('.checkbox-small').addClass("checked-lines");
         $('.checkbox-small').removeClass("checked-curve");
         $('.checkbox-small').removeClass("checked-half");
         $('.checkbox-small').removeClass("checked-circle");
+        $('.chart-row').addClass("barChart");
+        $('.chart-row').removeClass("curveChart");
     });
 
     $('#customCheck-curve:input').click(function(){
@@ -128,6 +234,8 @@ $(document).ready(function () {
         $('.checkbox-small').removeClass("checked-lines");
         $('.checkbox-small').removeClass("checked-half");
         $('.checkbox-small').removeClass("checked-circle");
+        $('.chart-row').addClass("curveChart");
+        $('.chart-row').removeClass("barChart");
     });
 
     $('#customCheck-half:input').click(function(){
