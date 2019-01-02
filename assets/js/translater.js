78a86332abe75370ffdcecb5fab39edf90e159e1
lahla <lahla81@yahoo.com>
2019-01-02 23:46:14 +0200

    add translation section

--- assets/js/translater.js
+++ assets/js/translater.js
@@ -1,5 +1,85 @@
 $(document).ready(function () {
 
+    $('#translator01').click(function(){
+        $('.trans-data').hide();
+        $('.layer').show();
+        +$('#translater-data01').toggle();
+    });
+
+    $('#translator02').click(function(){
+        $('.trans-data').hide();
+        $('.layer').show();
+        +$('#translater-data02').toggle();
+    });
+
+    $('#translator03').click(function(){
+        $('.trans-data').hide();
+        $('.layer').show();
+        +$('#translater-data03').toggle();
+        // $('#translater-data03').css({left:'0'});
+    });
+
+    $('#translator04').click(function(){
+        $('.trans-data').hide();
+        $('.layer').show();
+        +$('#translater-data04').toggle();
+    });
+
+    $('#translator05').click(function(){
+        $('.trans-data').hide();
+        $('.layer').show();
+        +$('#translater-data05').toggle();
+    });
+
+    $('#translator06').click(function(){
+        $('.trans-data').hide();
+        $('.layer').show();
+        +$('#translater-data06').toggle();
+    });
+
+    $('#translator07').click(function(){
+        $('.trans-data').hide();
+        $('.layer').show();
+        +$('#translater-data07').toggle();
+    });
+
+    $('#translator08').click(function(){
+        $('.trans-data').hide();
+        $('.layer').show();
+        +$('#translater-data08').toggle();
+    });
+
+    $('#translator09').click(function(){
+        $('.trans-data').hide();
+        $('.layer').show();
+        +$('#translater-data09').toggle();
+    });
+
+    $('#translator10').click(function(){
+        $('.trans-data').hide();
+        $('.layer').show();
+        +$('#translater-data10').toggle();
+    });
+
+    $('#translator11').click(function(){
+        $('.trans-data').hide();
+        $('.layer').show();
+        +$('#translater-data11').toggle();
+    });
+
+    $('#translator12').click(function(){
+        $('.trans-data').hide();
+        $('.layer').show();
+        +$('#translater-data12').toggle();
+    });
+
+    $('.layer').click(function(){
+        $('.trans-data').hide();
+        $('.layer').hide();
+    });
+
+    // $('.lawyer0_hide').css({display:'none'});
+
     var advertise_data = document.getElementById("bar-chart");
     const veto_data   = JSON.parse(advertise_data.parentElement.dataset.veto);
     const labels = JSON.parse(advertise_data.parentElement.dataset.labels);
